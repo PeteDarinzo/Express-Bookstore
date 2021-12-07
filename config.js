09b10ec4cece5ad84fd9ab460487e890831bddf2
Peter Darinzo <birel44@yahoo.com>
2021-12-07 10:54:27 -0700

    fix config

--- config.js
+++ config.js
@@ -4,13 +4,13 @@
 
 /*** Springboard Method ***/
 
-// let DB_URI = `postgresql://`;
+let DB_URI = `postgresql://`;
 
-// if (process.env.NODE_ENV === "test") {
-//   DB_URI = `${DB_URI}/books-test`;
-// } else {
-//   DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
-// }
+if (process.env.NODE_ENV === "test") {
+  DB_URI = `${DB_URI}/books-test`;
+} else {
+  DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
+}
 
 /*** End Springboard Method ***/
 
@@ -18,14 +18,14 @@
 
 /*** My Method ***/
 
-let DB_URI = {
-  host: "localhost",
-  user: "pete", // your username 
-  password: "vampire", // your password
-  database: "" // LEAVE BLANK
-}
+// let DB_URI = {
+//   host: "localhost",
+//   user: "", // your username 
+//   password: "", // your password
+//   database: "" // LEAVE BLANK
+// }
 
-DB_URI.database = (process.env.NODE_ENV === "test") ? "books_test" : "books";
+// DB_URI.database = (process.env.NODE_ENV === "test") ? "books_test" : "books";
 
 /*** End my method ***/
 
